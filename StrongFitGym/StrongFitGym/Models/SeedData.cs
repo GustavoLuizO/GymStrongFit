8ba9e7c4ce52e44b541de56142ef5ce8d4ac6689
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2022-03-28 13:19:45 -0300

    Create SeedData.cs
    Apenas para realizar a atividade do professor, mas o seed data nao é necessario

---
+++ StrongFitGym/StrongFitGym/Models/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace StrongFitGym.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            Contexto context = app.ApplicationServices.GetRequiredService<Contexto>();
+
+            DateTime date = DateTime.Now;
+
+            context.Database.Migrate();
+
+            if (!context.Personals.Any())
+            {
+                context.Personals.AddRange(
+                    new Personal
+                    {
+                        Name = "Fabricio",
+                        Speciality = "Pernas"
+                    });
+
+                context.Exercises.AddRange(
+                    new Exercise
+                    {
+                        Name = "Supino",
+                        Category = "Peito",
+                        Description = "Levanta barra"
+                    });
+
+                context.Trainings.AddRange(
+                    new Training
+                    {
+                        StudentID = 1,
+                        Date = date,
+                        Hour = date
+                    });
+                context.Students.AddRange(
+                    new Student
+                    {
+                        Name = "Gustavo",
+                        birthdate = date,
+                        Email = "X",
+                        Instagram = "X",
+                        Phone = "X",
+                        Comments = "X",
+                        PersonalID = 1
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
