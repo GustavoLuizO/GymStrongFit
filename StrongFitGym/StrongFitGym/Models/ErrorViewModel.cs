def5cfdefc6f8b6b242fa64b05758fe00d146844
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2022-03-28 12:55:07 -0300

    Commit Inicial

---
+++ StrongFitGym/StrongFitGym/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StrongFitGym.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
