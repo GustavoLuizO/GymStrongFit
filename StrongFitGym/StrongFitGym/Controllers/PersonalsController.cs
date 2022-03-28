377916bd3732abd1071599494fd5123d64d05b79
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2022-03-28 12:57:01 -0300

    Criação crontrollers
    Criação dos controllers Exercises, Personals, Students e Trainings

---
+++ StrongFitGym/StrongFitGym/Controllers/PersonalsController.cs
@@ -0,0 +1,153 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StrongFitGym.Models;
+
+namespace StrongFitGym.Controllers
+{
+    public class PersonalsController : Controller
+    {
+        private readonly Contexto _context;
+
+        public PersonalsController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Personals
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Personals.ToListAsync());
+        }
+
+        // GET: Personals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personal = await _context.Personals
+                .FirstOrDefaultAsync(m => m.PersonalID == id);
+            if (personal == null)
+            {
+                return NotFound();
+            }
+
+            return View(personal);
+        }
+
+        // GET: Personals/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PersonalID,Name,Speciality")] Personal personal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(personal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(personal);
+        }
+
+        // GET: Personals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personal = await _context.Personals.FindAsync(id);
+            if (personal == null)
+            {
+                return NotFound();
+            }
+            return View(personal);
+        }
+
+        // POST: Personals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PersonalID,Name,Speciality")] Personal personal)
+        {
+            if (id != personal.PersonalID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(personal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonalExists(personal.PersonalID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(personal);
+        }
+
+        // GET: Personals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personal = await _context.Personals
+                .FirstOrDefaultAsync(m => m.PersonalID == id);
+            if (personal == null)
+            {
+                return NotFound();
+            }
+
+            return View(personal);
+        }
+
+        // POST: Personals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var personal = await _context.Personals.FindAsync(id);
+            _context.Personals.Remove(personal);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonalExists(int id)
+        {
+            return _context.Personals.Any(e => e.PersonalID == id);
+        }
+    }
+}
