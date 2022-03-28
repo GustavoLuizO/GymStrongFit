93ea8b772e2eb51286ac55c213ade7fe6386df07
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2022-03-28 12:57:16 -0300

    Criação Migrations

---
+++ StrongFitGym/StrongFitGym/Migrations/20220328153012_Inicial-criacao.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StrongFitGym.Migrations
+{
+    public partial class Inicialcriacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exercises",
+                columns: table => new
+                {
+                    ExerciseID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercises", x => x.ExerciseID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personals",
+                columns: table => new
+                {
+                    PersonalID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Speciality = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personals", x => x.PersonalID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    StudentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Instagram = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonalID = table.Column<int>(type: "int", nullable: false),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.StudentID);
+                    table.ForeignKey(
+                        name: "FK_Student_Personals_PersonalID",
+                        column: x => x.PersonalID,
+                        principalTable: "Personals",
+                        principalColumn: "PersonalID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trainings",
+                columns: table => new
+                {
+                    TrainingID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentID = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Hour = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainings", x => x.TrainingID);
+                    table.ForeignKey(
+                        name: "FK_Trainings_Student_StudentID",
+                        column: x => x.StudentID,
+                        principalTable: "Student",
+                        principalColumn: "StudentID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExerciseTraining",
+                columns: table => new
+                {
+                    ExercisesExerciseID = table.Column<int>(type: "int", nullable: false),
+                    TrainingsTrainingID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExerciseTraining", x => new { x.ExercisesExerciseID, x.TrainingsTrainingID });
+                    table.ForeignKey(
+                        name: "FK_ExerciseTraining_Exercises_ExercisesExerciseID",
+                        column: x => x.ExercisesExerciseID,
+                        principalTable: "Exercises",
+                        principalColumn: "ExerciseID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExerciseTraining_Trainings_TrainingsTrainingID",
+                        column: x => x.TrainingsTrainingID,
+                        principalTable: "Trainings",
+                        principalColumn: "TrainingID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExerciseTraining_TrainingsTrainingID",
+                table: "ExerciseTraining",
+                column: "TrainingsTrainingID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_PersonalID",
+                table: "Student",
+                column: "PersonalID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trainings_StudentID",
+                table: "Trainings",
+                column: "StudentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExerciseTraining");
+
+            migrationBuilder.DropTable(
+                name: "Exercises");
+
+            migrationBuilder.DropTable(
+                name: "Trainings");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Personals");
+        }
+    }
+}
